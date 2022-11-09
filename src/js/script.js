0f81b4d8e71d98aa9f93ea35caf9fbbea0d1e46e
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2022-11-09 14:05:44 -0500

    fix functionality for toggle button

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,26 @@
 //@ts-check
 
 const inputs = document.querySelectorAll('.toggle__switch input');
+const labels = document.querySelectorAll('.toggle__switch label');
 
 if (inputs.length !== 0) {
   inputs.forEach((input) => {
-    input.addEventListener('click', () => {
-      if (typeof input.getAttribute('checked') !== 'string') {
-        inputs.forEach((input) => input.removeAttribute('checked'));
+    const label = input.parentElement;
 
-        input.toggleAttribute('checked');
-        input.parentElement?.classList.toggle('sr-only');
+    input.addEventListener('click', () => {
+      if (label !== null) {
+        if (label.dataset.hidden === 'true') {
+          label.dataset.hidden = 'false';
+        } else {
+          labels.forEach((label) => {
+            if (label instanceof HTMLLabelElement) {
+              label.dataset.hidden = 'false';
+            }
+          });
+
+          label.dataset.hidden = 'true';
+          label.firstElementChild?.toggleAttribute('checked');
+        }
       }
     });
   });
